781a8e41381db546c5279d5ed1418cf6be04d98c
Eric Gil <egc20@alumnes.udl.cat>
2024-12-10 10:06:28 +0100

    SERVICES SENSE APLICAR

---
+++ services/Server.java
@@ -0,0 +1,34 @@
+package services;
+//External services involved in the shared micromobility system
+import data.GeographicPoint;
+import data.StationID;
+import data.UserAccount;
+import data.VehicleID;
+
+import java.math.BigDecimal;
+import java.net.ConnectException;
+import java.time.LocalDateTime;
+
+public interface Server { // External service for the persistent storage
+
+    // To be invoked by the use case controller
+
+    void checkPMVAvail(VehicleID vhID)
+            throws PMVNotAvailException, ConnectException;
+
+    void registerPairing(UserAccount user, VehicleID veh, StationID st, GeographicPoint loc, LocalDateTime date)
+            throws InvalidPairingArgsException, ConnectException;
+
+    void stopPairing(UserAccount user, VehicleID veh, StationID st,
+                     GeographicPoint loc, LocalDateTime date, float avSp, float dist, int dur, BigDecimal imp)
+            throws InvalidPairingArgsException, ConnectException;
+
+    // Internal operations
+
+    void setPairing(UserAccount user, VehicleID veh, StationID st, GeographicPoint loc, LocalDateTime date)
+
+    void unPairRegisterService(JourneyService s)
+            throws PairingNotFoundException;
+    void registerLocation(VehicleID veh, StationID st);
+
+}
